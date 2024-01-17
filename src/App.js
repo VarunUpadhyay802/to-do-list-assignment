c9f0bdee1c6dc2f4d49a430dd4b95ffa45821b44
varun__upadhyay <varun.802.vu@gmail.com>
2024-01-17 10:58:26 +0530

    tailwind , mantine setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useElementSize } from '@mantine/hooks';
+import { rem } from '@mantine/core';
 
 function App() {
+  const { ref, width, height } = useElementSize();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-bold">
+     <textarea ref={ref} style={{ width: rem(400), height: rem(120) }} />
+      <div>Width: {width}, height: {height}</div>
     </div>
   );
 }
