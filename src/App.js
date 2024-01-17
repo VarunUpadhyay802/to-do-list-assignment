c5514a477e19487e2c6e2635faa3a79fe73ea6b9
varun__upadhyay <varun.802.vu@gmail.com>
2024-01-17 12:31:27 +0530

    setting the components up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { TodoProvider } from './context/TodoContext'
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
 const App = () => {
   return (
 	<div>
-	
+	<TodoProvider>
+			<div className="container">
+				<h1 className='app-title'>Todo Application</h1>
+				<AddTodo />
+				<TodoList />
+			</div>
+		</TodoProvider>
 	</div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>
+      fsd
+    </div>
+  )
+}
+
+export default TodoList
